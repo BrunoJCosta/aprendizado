f38767cd3b1dfb36aa4153757194dc0deb9514b2
BrunoJCosta <brunojereissati00@gmail.com>
2021-10-14 20:15:38 -0300

    nomeando pastas

--- src/main/java/com/aplication/teste/controller/CpfFourDevController.java
+++ src/main/java/com/aplication/teste/controller/CpfFourDevController.java
@@ -1,5 +1,6 @@
 package com.aplication.teste.controller;
 
+import com.aplication.teste.requests.FourDev;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
