67e517cc702c8ba4795b93e4a9fa8c787f7263a2
BrunoJCosta <brunojereissati00@gmail.com>
2022-03-13 17:48:24 -0300

    colocando os script de criação da tabela no schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,1 +1,23 @@
-select * from aluno;
+create table if not exists aluno(
+    id bigint not null primary key,
+    nome varchar(255),
+    matricula varchar(9),
+    materia_id bigInt,
+    date_matricula timestamp default current_timestamp
+);
+
+create table if not exists materia(
+    id bigint not null primary key,
+    nome varchar(255),
+    matricula varchar(9),
+    creditos int,
+    professor_id bigInt
+);
+
+create table if not exists professor(
+    id bigint not null primary key,
+    nome varchar(255),
+    aula varchar(50),
+    formacao varchar(20),
+    salario decimal(5,3)
+);
